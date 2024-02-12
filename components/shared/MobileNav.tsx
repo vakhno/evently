8fdcaba188115368030b48b7b6dea2453e28498f
Vakhno <vakhnohere@gmail.com>
2024-02-12 12:24:45 +0200

    Editing menu side bar and adding footer

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -9,7 +9,9 @@ import {
 } from '@/components/ui/sheet';
 import Image from 'next/image';
 import Menu from '@/public/assets/icons/menu.svg';
-
+import Logo from '@/public/assets/images/logo.svg';
+import { Separator } from '../ui/separator';
+import NavItems from './NavItems';
 type Props = {};
 
 const MobileNav = (props: Props) => {
@@ -17,16 +19,12 @@ const MobileNav = (props: Props) => {
 		<nav className="md:hidden">
 			<Sheet>
 				<SheetTrigger className="align-middle">
-					<Image src="" width={24} height={24} className="cursor-pointer" alt="menu" />
+					<Image src={Menu} width={24} height={24} className="cursor-pointer" alt="menu" />
 				</SheetTrigger>
-				<SheetContent>
-					<SheetHeader>
-						<SheetTitle>Are you absolutely sure?</SheetTitle>
-						<SheetDescription>
-							This action cannot be undone. This will permanently delete your account and remove
-							your data from our servers.
-						</SheetDescription>
-					</SheetHeader>
+				<SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+					<Image src={Logo} alt="logo" width={128} height={38} />
+					<Separator className="border border-gray-50" />
+					<NavItems />
 				</SheetContent>
 			</Sheet>
 		</nav>
